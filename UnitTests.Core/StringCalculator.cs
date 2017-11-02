fa9e4674dab849baa737e5426e8b3ef4f1a8a4ed
rafaelbittenca <rafaelbittencourt.fns@gmail.com>
2017-11-02 18:16:15 -0400

    New tests

---
+++ UnitTests.Core/StringCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UnitTests.Core
+{
+	public class StringCalculator
+	{
+		public int Add(string inputValue)
+		{
+
+			if (string.IsNullOrEmpty(inputValue)) return 0;
+
+			var numbers = inputValue.Split(',');
+			var total = 0;
+			foreach(var number in numbers)
+			{
+				total += TryparseToInteger(number);
+			}
+			return total;
+		}
+
+		private int TryparseToInteger(string input)
+		{
+			int dest;
+			if (!int.TryParse(input, out dest))
+			{
+				throw new ArgumentException("Input format was incorrect");
+			}
+			return dest;
+		}
+	}
+}
--- UnitTests/Learn_UnitTest.cs
+++ UnitTests/Learn_UnitTest.cs
@@ -1,17 +1,56 @@
 ﻿
 using NUnit.Framework;
+using System;
+using UnitTests.Core;
 
 namespace UnitTests
 {
 	public class Learn_UnitTest
 	{
+		private StringCalculator GetCalculator()
+		{
+			var calc = new StringCalculator();
+			return calc;
+		}
+
 		[Test]
 		public void Add_EmptyString_Returns0()
 		{
-			StringCalculator calc = new StringCalculator();
+			StringCalculator calc = GetCalculator();
 			int expectedResult = 0;
 			int result = calc.Add("");
 			Assert.AreEqual(expectedResult, result);
+		}
+
+		[TestCase("1", 1)]
+		[TestCase("2", 2)]
+		[TestCase("9", 9)]
+		[TestCase("10", 10)]
+		public void Add_SingleNumbers_ReturnsTheNumber(string input, int expectedResult)
+		{
+			StringCalculator calc = GetCalculator();
+			int result = calc.Add(input);
+			Assert.AreEqual(expectedResult, result);
+		}
+		
+		[TestCase("110,11", 121)]
+		[TestCase("5,3,2", 10)]
+		[TestCase("5,1,3", 9)]
+		[TestCase("25,30", 55)]
+		public void Add_Multiple_SumOfAll(string input, int expectedResult)
+		{
+			StringCalculator calc = GetCalculator();
+			int result = calc.Add(input);
+			Assert.AreEqual(expectedResult, result);
+		}
+
+		[Test]
+		public void Add_InvalidString_ThrowException()
+		{
+			StringCalculator calc = GetCalculator();
+			string input = "a,1";
+			//calc.Add(input);
+			Assert.That(() => calc.Add(input), Throws.TypeOf<ArgumentException>());
 		}
 	}
 }
